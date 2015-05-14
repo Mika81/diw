5ecb1c6962ac542a316413b245627b36e4401e38
Mika <webdeveloper@diw.fr>
2015-05-14 09:22:09 +0200

    adds correstions to Reader and Item classes

--- model/rss/Reader.php
+++ model/rss/Reader.php
@@ -29,11 +29,26 @@ class Reader {
                 ->setDescription($xmlItem->description)
                 ->setLink($xmlItem->link)
                 ->setDate(new \Datetime($xmlItem->pubDate))
-                ->setPicture($xmlItem->thumbnail['url'])
+//                ->setPicture($xmlItem->thumbnail['url'])
                 ->setAuthor($xmlItem->author);
             array_push($items, $item);
         }
         return $items;
+    }
+    
+    public function reader()
+    {
+        $items = $this->xml;
+        
+        foreach($items->channel as $channel){
+            echo $channel->lastBuildDate . " :<br/><br/>";
+            foreach($channel->item as $item){
+                echo "<strong>".$item[0]->category. "</strong><br/>";
+                echo $item->title. "<br/>";
+                echo "<i>".$item->description. "</i><br/>";
+                echo "<br/>";
+            }
+        }
     }
 
 }
