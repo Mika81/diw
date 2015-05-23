97c3d711da635cf893f80b586e801ea1a21cdfc5
Mika <webdeveloper@diw.fr>
2015-05-23 20:07:13 +0200

    Modifies homepage template

--- views/home.php
+++ views/home.php
@@ -9,23 +9,47 @@
         <link href="/src/css/bootstrap/css/flatly.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div class="container-fluid">
+        <!-- Starts header -->
+        <header>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">
+                            <img alt="logo" src="...">
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- Ends header -->
+        <!-- Starts section -->
+        <section class="col-xs-12 col-md-8 container-fluid">
             <div class="row">
-                <div class="col-xs-12 col-md-8">
+                <div>
                     <h1>Actualités Web</h1>
-                    <?php foreach($items as $item) : ?>
-                        <article>
-                            <a href="<?= $item->getLink(); ?>"><h2><?= $item->getTitle(); ?></h2></a>
-                            <div><img src="<?= $item->getPicture(); ?>"/></div>
-                            <p><?= $item->getDate()->format('d/m/Y'); ?></p>
-                            <p><?= $item->getDescription(); ?></p>
+                    <?php foreach ($items as $item) : ?>
+                        <article class="media jumbotron">
+                            <div class="media-body">
+                                <p><?= $item->getDescription(); ?></p>
+                            </div>
+                            <div class='pull-left'>
+                                <h5 class="text-info">Article du <?= $item->getDate()->format('d/m/Y'); ?></h5>
+                            </div>
+                            <div class='text-right'>
+                                <a href="<?= $item->getLink(); ?>" target="_blank" class='btn btn-primary btn-lg'>Lire l'article</a>
+                            </div>
                         </article>
                     <?php endforeach ?>
                 </div>
-                <div class="col-xs-6 col-md-4">
-                    <h1>Ma colonne de droite</h1>
-                </div>
             </div>
-        </div>
-    </body>
+        </section>
+        <!-- Ends section -->
+        <!-- Starts aside -->
+        <aside class="col-xs-6 col-md-4 container-fluid">
+            <div class="row">
+                <h1>Ma colonne de droite</h1>
+            </div>
+        </aside>
+        <!-- Ends aside -->
+</body>
 </html>
