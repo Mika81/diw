f69891e41954b0a665e5b0d4ddacc7687a75a6d5
Mika <webdeveloper@diw.fr>
2015-05-31 19:01:59 +0200

    Modifies home template to include head, article and footer via another file .php

--- views/home.php
+++ views/home.php
@@ -1,55 +1,19 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>DIW.fr | News</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="/src/css/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="/src/css/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
-        <link href="/src/css/bootstrap/css/flatly.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <!-- Starts header -->
-        <header>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="#">
-                            <img alt="logo" src="...">
-                        </a>
-                    </div>
-                </div>
-            </nav>
-        </header>
-        <!-- Ends header -->
-        <!-- Starts section -->
-        <section class="col-xs-12 col-md-8 container-fluid">
-            <div class="row">
-                <div>
-                    <h1>Actualités Web</h1>
-                    <?php foreach ($items as $item) : ?>
-                        <article class="media jumbotron">
-                            <div class="media-body">
-                                <p><?= $item->getDescription(); ?></p>
-                            </div>
-                            <div class='pull-left'>
-                                <h5 class="text-info">Article du <?= $item->getDate()->format('d/m/Y'); ?></h5>
-                            </div>
-                            <div class='text-right'>
-                                <a href="<?= $item->getLink(); ?>" target="_blank" class='btn btn-primary btn-lg'>Lire l'article</a>
-                            </div>
-                        </article>
-                    <?php endforeach ?>
-                </div>
-            </div>
-        </section>
-        <!-- Ends section -->
-        <!-- Starts aside -->
-        <aside class="col-xs-6 col-md-4 container-fluid">
-            <div class="row">
-                <h1>Ma colonne de droite</h1>
-            </div>
-        </aside>
-        <!-- Ends aside -->
-</body>
-</html>
+<?php include_once 'head.inc.php'; ?>
+
+<div class='container-fluid'>
+    <!-- Starts section -->
+    <section class="col-xs-12 col-md-8 container-fluid b_r_1_silver">
+        <div class ="m_top_50 p_30">
+            <?php include_once 'articles.inc.php'; ?>      
+        </div>
+    </section>
+    <!-- Ends section -->
+    
+    <!-- Starts aside -->
+    <?php include_once 'aside.inc.php'; ?>
+    <!-- Ends aside -->
+    
+</div>
+<!-- Ends .container-fluid -->
+
+<?php include_once 'footer.inc.php'; ?>
