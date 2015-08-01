faa27aa831e0adf57a8fb62e1916e32e3327c850
Mika <webdeveloper@diw.fr>
2015-08-01 19:19:40 +0200

    Modifies article template to integrate variations from differents rss sources - Creates source.php file to clarify code

---
+++ src/rss/source.php
@@ -0,0 +1,29 @@
+<?php
+
+switch ($source) {
+    case "http://www.journaldunet.com/web-tech/developpeur/":
+        $display = "jdn";
+        $site_name = "Journal du net";
+        $url = "http://www.journaldunet.com/web-tech/developpeur/";
+        break;
+
+    case "http://web.developpez.com/index/rss":
+        $display = "dev.com";
+        $site_name = "Développez.com";
+        $url = "http://web.developpez.com/";
+        break;
+
+    case "http://www.internetactu.net":
+        $display = "internetactu";
+        $site_name = "Internet Actu";
+        $url = "http://www.internetactu.net";
+        break;
+    
+    case "http://www.zdnet.fr/feeds/rss/actualites/internet/":
+        $display = "zdnet";
+        $site_name = "ZDnet.fr";
+        $url = "http://www.internetactu.net";
+        break;
+        
+}  
+
--- views/articles.inc.php
+++ views/articles.inc.php
@@ -1,13 +1,21 @@
-<?php foreach ($items as $item) : ?>
+<?php 
+include_once "src/rss/source.php";
+
+foreach ($items as $item) : ?>
     <article class="media jumbotron">
-        <div class="media-body">
+        <?php if($display == "jdn" || $display == "internetactu" || $display == "zdnet"){ ?>
+        <div class="media-heading">
+            <p><strong><?= $item->getTitle(); ?></strong></p>
+        </div>
+        <?php } ?>
+        <div class="media-body">            
             <p><?= $item->getDescription(); ?></p>
         </div>
         <div class='pull-left'>
             <h5 class="text-info">Article du <?= $item->getDate()->format('d/m/Y'); ?></h5>
         </div>
         <div class='text-right'>
-            <a href="<?= $item->getLink(); ?>" target="_blank" class='btn btn-info btn-lg'>Lire l'article</a>
+            <a href="<?= $item->getLink(); ?>" target="_blank" class='btn btn-info btn-lg'>Lire l'article sur "<?php echo $site_name; ?>"</a>
         </div>
     </article>
 <?php endforeach ?>
