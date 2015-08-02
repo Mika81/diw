e82ad55b8ce35ef7c3112495835a7191c1a995c9
Mika <webdeveloper@diw.fr>
2015-08-02 11:24:48 +0200

    modifies index file in aim to integrate all modifications (change files required, variables, objects...)

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
-require_once 'src/rss/Item.php';
-require_once 'src/rss/Reader.php';
-require_once 'src/rss/filename.php';
+require_once 'src/functions/functions.php';
+require_once 'src/rss/Item.class.php';
+require_once 'src/rss/Reader.class.php';
+require_once 'src/rss/Filename.class.php';
 
+$set_post_source = set_Post_Source();
+$post_source_object = new \src\rss\Filename($set_post_source);
+$post_source = $post_source_object->post_source;
+
+$filename = $post_source_object->switch_post_source($post_source);
 $reader = new \src\rss\Reader($filename);
 $source = $reader->reader();
 $items = $reader->getItems();
